d4c42585722bf807e12d0ca502cd712256eaadc6
everito <everardo.ipn@gmail.com>
2014-08-17 22:24:03 -0500

    Primer commit del sistema, solo contiene menu

---
+++ app/views/header.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <link rel="stylesheet" href="css/style.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Adhesivoshenkel</title>
+</head>
+<body>
+
+	<nav id="menu">
+	<ul>
+		<li><span>Inicio</span></li>
+		<li><span>Ventas/Compras</span>
+			<ul>
+				<li><span>Ventas</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Compras</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Clientes</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Proveedores</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li><span>Productos</span>
+			<ul>
+				<li><span>Productos</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Presentaciones</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Precios de Productos</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Precios de envases</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Cortes de inventario</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Reporte de inventario</span></li>
+				<li><span>Reporte de productos vendidos</span></li>
+			</ul>
+		</li>
+		<li><span>Cobros</span>
+			<ul>
+				<li><span>Cobros</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Formas de pago</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+				<li><span>Condiciones de pago</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li><span>Gastos</span>
+			<ul>
+				<li><span>Gastos</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+			</ul>
+			<ul>
+				<li><span>Rubos de gastos</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li><span>Otros</span>
+			<ul>
+				<li><span>Asuntos pendientes</span>
+					<ul>
+						<li><a href="#">Nuevo</a></li>
+						<li><a href="#">Buscar/Reporte</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+	</nav>
+
+
+
+ @yield('content')
+
+</body>
+<html>
---
+++ app/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('header')
+
+@section('content')
+
+<form action="">
+	<div>
+		<lable></lable>
+		<input type="text">
+	</div>
+	<div>
+		<lable></lable>
+		<input type="text">
+	</div>
+	<div>
+		<lable></lable>
+		<input type="text">
+	</div>
+</form>
+
+@stop
